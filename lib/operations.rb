f3bb043052591f24446b005c7a1d1776f83e8343
Jessi Poulson <jessipoulson@hotmail.com>
2020-09-13 09:24:10 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,3 +1,5 @@
+require "pry"
+
 def unsafe?(speed)
   if speed > 60
     true
@@ -11,5 +13,6 @@ end
 
 def not_safe?(speed)
 	speed < 40 ? true : false
+	binding.pry
 	speed > 60 ? true : false
 end
